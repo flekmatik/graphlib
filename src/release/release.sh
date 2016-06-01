7b4c4685623e0c2710b8576c36cae612766c8a40
Richard Karmazin <richard.karmazin@solarwinds.com>
2016-06-01 10:20:22 +0200

    hacking custom release

--- src/release/release.sh
+++ src/release/release.sh
@@ -26,36 +26,11 @@ echo Attemping to publish version: $VERSION
 [ "`git rev-parse HEAD`" = "`git rev-parse master`" ] || [ -n "$PRE_RELEASE" ] || bail "ERROR: You must release from the master branch"
 [ -z "`git status --porcelain`" ] || bail "ERROR: Dirty index on working tree. Use git status to check"
 
-# Publish to pages
-rm -rf $PAGES_DIR
-git clone git@github.com:cpettitt/cpettitt.github.com.git $PAGES_DIR
-
-TMP_TARGET=$PAGES_DIR/project/$PROJECT/latest
-rm -rf $TMP_TARGET
-mkdir -p $TMP_TARGET
-cp -r $DIST_DIR/*.js $TMP_TARGET
-
-TMP_TARGET=$PAGES_DIR/project/$PROJECT/v$VERSION
-rm -rf $TMP_TARGET
-mkdir -p $TMP_TARGET
-cp -r $DIST_DIR/*.js $TMP_TARGET
-
-cd $PAGES_DIR/project/$PROJECT
-git add -A
-git commit -m "Publishing $PROJECT v$VERSION"
-git push -f origin master
-cd $PROJECT_ROOT
-echo "Published $PROJECT to pages"
-
 # Publish tag
 git tag v$VERSION
 git push origin
 git push origin v$VERSION
 echo Published $PROJECT v$VERSION
-
-# Publish to npm
-npm publish
-echo Published to npm
 
 # Update patch level version + commit
 ./src/release/bump-version.js
